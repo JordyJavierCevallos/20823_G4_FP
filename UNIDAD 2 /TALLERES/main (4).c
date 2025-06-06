34943c5c8b8fcc07f5a0c2b760528f542d6675b7
BryanMiguitamaManuelAjila <bmmiguitama@espe.edu.ec>
2025-06-06 08:42:18 -0500

    Add files via upload

---
+++ UNIDAD 2 /TALLERES/main (4).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[10][10];
+    int i, j, suma = 0, adivinanza;
+
+    printf("Ingrese los valores de la matriz 10x10:\n");
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("Elemento [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+            suma += matriz[i][j];
+        }
+    }
+
+    printf("Adivine la suma total de los elementos de la matriz: ");
+    scanf("%d", &adivinanza);
+
+    if (adivinanza == suma) {
+        printf("¡Correcto! La suma es %d\n", suma);
+    } else {
+        printf("Incorrecto. La suma correcta es %d\n", suma);
+    }
+
+    return 0;
+}
