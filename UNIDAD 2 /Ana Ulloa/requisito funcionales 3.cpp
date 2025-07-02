0a07408b205b46dffbeab5985d5d2ec31c7907c5
anasofiaulloanicolalde <asulloa1@espe.edu.ec>
2025-07-02 08:19:21 -0500

    Update requisito funcionales 3.cpp

--- UNIDAD 2 /Ana Ulloa/requisito funcionales 3.cpp
+++ UNIDAD 2 /Ana Ulloa/requisito funcionales 3.cpp
@@ -1,7 +1,7 @@
-// UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE//
-// ANA SOFIA ULLOA
-// 30/06/2025
-//NRC: 20823
+// UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+// NOMBRE: ANA ULLOA
+// NRC:20823
+// FECHA: 30/06/2025
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +12,7 @@
 int main() {
     int minimo, maximo;
 
-    // RF01 - Configuración dinámica del rango
-    printf("Ingrese el valor minimo : ");
+    printf("Ingrese el valor minimo : ");  // RF01 - Configuración dinámica del rango
     scanf("%d", &minimo);
     printf("Ingrese el valor maximo : ");
     scanf("%d", &maximo);
@@ -25,8 +24,7 @@ int main() {
 
     int intentosPermitidos;
 
-    // RF02 - Configuracion dinámica de intentos
-    do {
+    do {  // RF02 - Configuración dinámica de intentos
         printf("Ingrese la cantidad de intentos (max %d): ", MAX_INTENTOS);
         scanf("%d", &intentosPermitidos);
         if (intentosPermitidos < 1 || intentosPermitidos > MAX_INTENTOS) {
@@ -34,13 +32,7 @@ int main() {
         }
     } while (intentosPermitidos < 1 || intentosPermitidos > MAX_INTENTOS);
 
-    // RF04 - Registro de intentos en matriz
-    int intentos[MAX_INTENTOS][3];
-    for (int i = 0; i < MAX_INTENTOS; i++) {
-        intentos[i][0] = i + 1;
-        intentos[i][1] = 0;    // inicia en 0
-        intentos[i][2] = -1;   
-    }
+    int intentos[MAX_INTENTOS][3];  // RF04 - Registro de intentos en matriz
 
     int numeroSecreto;
     int adivinado = 0;
@@ -49,18 +41,16 @@ int main() {
     srand(time(NULL));
     numeroSecreto = rand() % (maximo - minimo + 1) + minimo;
 
-    for (int i = 0; i < intentosPermitidos; ) {
+    for (int i = 0; i < intentosPermitidos;) {
         int intento;
         printf("Intento %d: Ingresa un numero entre %d y %d: ", i + 1, minimo, maximo);
         scanf("%d", &intento);
 
-        // RF03 - Validación de rango y duplicados
         if (intento < minimo || intento > maximo) {
             printf("Numero fuera del rango.\n");
             continue;
         }
 
-        // RF03 - Validación de rango y duplicados
         int repetido = 0;
         for (int j = 0; j < numIntentosValidos; j++) {
             if (intentos[j][1] == intento) {
@@ -73,53 +63,42 @@ int main() {
             continue;
         }
 
-        // Guardar intentos valiudos
+        intentos[i][0] = i + 1;
         intentos[i][1] = intento;
 
-        // Determina si el intento fue correcto si fue muy alto o muy bajo
         if (intento == numeroSecreto) {
-            intentos[i][2] = 2; // correcto
-            printf("\nAdivinaste, el numero secreto es %d .\n",numeroSecreto);
+            intentos[i][2] = 2;
+            printf("\nAdivinaste, el numero secreto es %d.\n", numeroSecreto);
             adivinado = 1;
             numIntentosValidos++;
             i++;
             break;
         } else if (intento < numeroSecreto) {
-            intentos[i][2] = 0; // bajo
-            printf("Muy bajo.\n");
+            intentos[i][2] = 0;
+            printf("bajo.\n");
         } else {
-            intentos[i][2] = 1; // alto
-            printf("Muy alto.\n");
+            intentos[i][2] = 1;
+            printf("alto.\n");
         }
 
         numIntentosValidos++;
         i++;
     }
 
-    // Rellena intentos restantes con 0 
-    for (int k = numIntentosValidos; k < intentosPermitidos; k++) {
-        intentos[k][1] = 0;
-        intentos[k][2] = -1; // indica que no fue un intento real
-    }
-
-    // RF05 -  Visualización de resumen detallado
-    printf("\nResumen detallado de intentos:\n");
+    printf("\nResumen:\n");  // RF05 - Visualización de resumen detallado
     printf("Intento | Valor  | Resultado\n");
-    printf("-------------------------------------\n");
-    for (int k = 0; k < intentosPermitidos; k++) {
+
+    for (int k = 0; k < numIntentosValidos; k++) {
         printf("%6d | %6d | ", intentos[k][0], intentos[k][1]);
         switch (intentos[k][2]) {
             case 0:
-                printf("Muy bajo\n");
+                printf("bajo\n");
                 break;
             case 1:
-                printf("muy alto\n");
+                printf("alto\n");
                 break;
             case 2:
                 printf("Correcto\n");
-                break;
-            case -1:
-                printf("Vacio\n");
                 break;
         }
     }
